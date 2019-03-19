6198c2999419249b6fae0392fdc7e777d8a7d2b2
liuxing <603670760@qq.com>
2019-03-19 13:03:19 +0800

    初始化微信公众号项目

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const app = express()
+
+app.use((req,res) => {
+  
+})
+
+app.listen(3000,err => {
+  if(!err) console.log('服务器连接成功')
+  else console.log(err)
+})
+
