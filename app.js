6c6f9fe364ffbaeac46bb63da1460a36d46a9f13
liuxing <603670760@qq.com>
2019-03-19 13:37:45 +0800

    '配置接口信息'

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require('express')
 
+const sha1 = require('sha1')
+
 const app = express()
 
 app.use((req,res) => {
-  
+  const {signature,echostr,timestamp,nonce} = req.query
+
+  const token = 'Wechat-liuxing'
+
+  const sortedArr = [token,timestamp,nonce].sort()
+
+  const sha1Str = sha1(sortedArr.join(''))
+
+  if(sha1Str === signature){
+    res.end(echostr)
+  }else{
+    res.end('error')
+  }
 })
 
-app.listen(3000,err => {
+app.listen(8888,err => {
   if(!err) console.log('服务器连接成功')
   else console.log(err)
 })
